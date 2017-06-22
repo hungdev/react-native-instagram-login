42e9906c8a96d0479d4f3101ba2954fd25703b31
hungdev <hungns126@gmail.com>
2017-06-22 16:30:12 +0700

    add logout

--- example/index.android.js
+++ example/index.android.js
@@ -14,19 +14,43 @@ import {
   TouchableOpacity
 } from 'react-native';
 import Ins from './Instagram'
+import Cookie from 'react-native-cookie';
+
 export default class InsLogin extends Component {
+
+  state = {
+
+  }
+
+  logout() {
+    Cookie.clear().then(() => {
+      this.setState({ token: null })
+    })
+
+  }
 
   render() {
     return (
-      <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-        <TouchableOpacity style={{borderRadius: 5, backgroundColor: 'orange', height:30, width: 100, justifyContent: 'center', alignItems: 'center'}} onPress={()=> this.refs.ins.show()}>
-          <Text style={{color: 'white'}}>Login</Text>
-        </TouchableOpacity>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        {!this.state.token ? (
+          <TouchableOpacity style={{ borderRadius: 5, backgroundColor: 'orange', height: 30, width: 100, justifyContent: 'center', alignItems: 'center' }} onPress={() => this.refs.ins.show()}>
+            <Text style={{ color: 'white' }}>Login</Text>
+          </TouchableOpacity>
+        ) : (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+              <Text style={{ margin: 10 }}>token: {this.state.token}</Text>
+              <TouchableOpacity style={{ borderRadius: 5, backgroundColor: 'green', height: 30, width: 100, justifyContent: 'center', alignItems: 'center' }} onPress={() => this.logout()}>
+                <Text style={{ color: 'white' }}>Logout</Text>
+              </TouchableOpacity>
+            </View>
+          )
+        }
         <Ins
           ref='ins'
           clientId='c7831304b961485a938674163166e606'
           redirectUrl='https://google.com'
           scopes={['public_content+follower_list']}
+          onLoginSuccess={(token) => this.setState({ token })}
         />
       </View>
     );
